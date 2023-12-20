40fd87943557c064d43dc8613f8ecee7ba24ad2c
gonzaloandresoto <gonzandresoto@gmail.com>
2023-12-20 15:16:59 -0500

    fixed case where no uncompleted or in/progress routes causes crash

--- components/CourseComponents/CourseCover.tsx
+++ components/CourseComponents/CourseCover.tsx
@@ -17,6 +17,13 @@ export default async function CourseCover({
 }: CourseCoverProps) {
   const nextUnit = await getNextUncompletedUnit(courseId);
 
+  let route;
+  if (!nextUnit.message) {
+    route = `/course/${courseId}/${nextUnit}`;
+  } else {
+    route = `/course/${courseId}`;
+  }
+
   return (
     <div className='fixed bottom-0 w-[720px] h-5/6 flex flex-col items-center px-8 pt-8 bg-tertiary-grey rounded-t-xl overflow-y-auto'>
       <div className='w-full h-max flex flex-col gap-8'>
@@ -28,7 +35,7 @@ export default async function CourseCover({
       </div>
 
       <Link
-        href={`/course/${courseId}/${nextUnit}`}
+        href={route}
         className='fixed bottom-16 main-button'
       >
         Start learning
