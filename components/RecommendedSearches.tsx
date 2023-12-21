3142ed50cdcdffcf94835fc2c6c323b23aeff377
gonzaloandresoto <gonzandresoto@gmail.com>
2023-12-21 17:23:41 -0500

    fixed missing constants

--- components/RecommendedSearches.tsx
+++ components/RecommendedSearches.tsx
@@ -1,9 +1,9 @@
-import { recommendedSearches } from '@/constants';
+// import { recommendedSearches } from '@/constants';
 
 export default function RecommendedSearches() {
   return (
     <div className='max-w-[880px] flex flex-wrap gap-4 justify-center'>
-      {recommendedSearches.map((item, index) => {
+      {/* {recommendedSearches.map((item, index) => {
         return (
           <button
             key={index}
@@ -12,7 +12,7 @@ export default function RecommendedSearches() {
             {item.term}
           </button>
         );
-      })}
+      })} */}
     </div>
   );
 }
