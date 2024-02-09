93362c132a55f3acd25b57a4da1f00f27ace345b
gonzaloandresoto <gonzandresoto@gmail.com>
2024-02-08 22:24:37 -0500

    added collapse action on links

--- components/Course/FurtherReading.tsx
+++ components/Course/FurtherReading.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { ListCollapse } from 'lucide-react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
+import { useState } from 'react';
 
 const links = [
   {
@@ -23,23 +25,35 @@ const links = [
 ];
 
 export default function FurtherReading() {
+  const [open, setOpen] = useState(false);
   const { unit } = useParams<{ unit: string }>() || {
     unit: '',
   };
   return (
-    <div className='w-full min-h-[144px] h-max p-4 general-container flex flex-col gap-4'>
-      <h2 className='h3 font-rosario'>Further Reading</h2>
-      <div className='grid gap-2'>
-        {links.map((link) => (
-          <Link
-            key={link.url}
-            href={link.url}
-            className='text-base text-tertiary-black hover:text-primary-blue font-rosario font-medium hover:underline'
-          >
-            {link.title}
-          </Link>
-        ))}
+    <div className='flex-none w-full h-max p-4 general-container flex flex-col gap-4'>
+      <div className='flex flex-row items-center justify-between'>
+        <h2 className='h3 font-rosario'>Further Reading</h2>
+        <button onClick={() => setOpen(!open)}>
+          <ListCollapse
+            size={18}
+            className='text-tertiary-black'
+          />
+        </button>
       </div>
+
+      {open && (
+        <div className='grid gap-2'>
+          {links.map((link) => (
+            <Link
+              key={link.url}
+              href={link.url}
+              className='text-base text-tertiary-black hover:text-primary-blue font-rosario font-medium hover:underline'
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
